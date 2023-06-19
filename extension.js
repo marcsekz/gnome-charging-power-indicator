c7bc286ff06789d7dc066278b16e6b4968a3ec5e
Zoltán Marcsek <marcsek.zoltan@gmail.com>
2023-06-19 17:45:21 +0200

    It works now, but it leaves un ugly gap when not charging

--- extension.js
+++ extension.js
@@ -3,52 +3,63 @@ const Main = imports.ui.main;
 const PanelMenu = imports.ui.panelMenu;
 const Lang = imports.lang;
 const Clutter = imports.gi.Clutter;
-const UPowerGlib = imports.gi.UPowerGlib;
+const Gio = imports.gi.Gio;
+const GLib = imports.gi.GLib;
+const GObject = imports.gi.GObject;
 
-const ChargingPowerIndicator = new Lang.Class({
-    Name: 'ChargingPowerIndicator',
-    Extends: PanelMenu.Button,
+var ChargingPowerIndicator = GObject.registerClass(
+    class ChargingPowerIndicator extends PanelMenu.Button {
+        _init() {
+            super._init(0.0, "Charging Power Indicator", false);
 
-    _init: function() {
-        this.parent(0.0, "Charging Power Indicator", false);
-        this._icon = new St.Icon({
-            icon_name: 'battery-full-charged-symbolic',
-            style_class: 'system-status-icon'
-        });
-        this.actor.add_child(this._icon);
+            this._icon = new St.Icon({
+                icon_name: 'battery-full-charged-symbolic',
+                style_class: 'system-status-icon'
+            });
+            // this.actor.add_child(this._icon);
 
-        this._powerLabel = new St.Label({
-            text: '0W',
-            y_expand: true,
-            y_align: Clutter.ActorAlign.CENTER
-        });
-        this.actor.add_child(this._powerLabel);
+            this._powerLabel = new St.Label({
+                text: '0W',
+                // y_expand: true,
+                y_align: Clutter.ActorAlign.CENTER
+            });
+            this.actor.add_child(this._powerLabel);
 
-        this._updatePower();
+            // this._updatePower();
 
-        let upClient = UPowerGlib.Client.new();
-        upClient.connect('notify::on-battery', Lang.bind(this, this._updatePower));
-    },
-
-    _updatePower: function() {
-        let upClient = UPowerGlib.Client.new();
-        let devices = upClient.get_devices();
-        let power = 0;
-
-        for (let i = 0; i < devices.length; i++) {
-            let device = devices[i];
-            if (device.is_present() && device.is_rechargeable() && device.get_state() === UPowerGlib.DeviceState.CHARGING) {
-                power += device.get_power();
-            }
+            // this._powerMonitor = Gio.File.new_for_path('/sys/class/power_supply/BAT0/power_now').monitor_file(Gio.FileMonitorFlags.NONE, null);
+            // this._powerMonitor.connect('changed', Lang.bind(this, this._updatePower));
         }
 
-        this._powerLabel.set_text(power.toFixed(2) + 'W');
-    },
+        _updatePower() {
+            let file1 = Gio.File.new_for_path('/sys/class/power_supply/BAT0/status');
+            let [, contents1] = file1.load_contents(null);
 
-    destroy: function() {
-        this.parent();
+            if (contents1.toString().trim() == "Charging")
+            {
+                let file = Gio.File.new_for_path('/sys/class/power_supply/BAT0/power_now');
+                let [, contents] = file.load_contents(null);
+
+                let power = parseFloat(contents.toString().trim()) / 1000000; // Convert to Watts
+                this._powerLabel.set_text(power.toFixed(2) + 'W');
+            }
+            else
+            {
+                // this._powerLabel.set_text(contents1.toString());
+                this._powerLabel.set_text('');
+            }
+
+            // this._powerLabel.set_text("Helllo")
+        }
+
+        destroy() {
+            this._powerMonitor.cancel();
+            this._powerMonitor = null;
+
+            super.destroy();
+        }
     }
-});
+);
 
 function init() {
     // initialization code
@@ -56,9 +67,15 @@ function init() {
 
 let chargingPowerIndicator;
 
+function update() {
+    chargingPowerIndicator._updatePower();
+    return GLib.SOURCE_CONTINUE;;
+}
+
 function enable() {
     chargingPowerIndicator = new ChargingPowerIndicator();
     Main.panel.addToStatusArea('charging-power-indicator', chargingPowerIndicator);
+    timeout=GLib.timeout_add(GLib.PRIORITY_DEFAULT_IDLE, 1000, update);
 }
 
 function disable() {
